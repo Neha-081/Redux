1e82f9553a03d8528a850be55aadc5297f7fff3a
Neha <nehaprajapati1150@gmail.com>
2022-01-17 00:08:11 +0530

    redux-thunk middleware

--- asyncActions.js
+++ asyncActions.js
@@ -1,6 +1,10 @@
 const redux=require('redux')
 const createStore=redux.createStore
+const applyMiddleware=redux.applyMiddleware
+const thunkMiddleware=require('redux-thunk').default
+const axios =require('axios')
 
+//state
 const initialState={
     loading:false,
     users:[],
@@ -34,7 +38,7 @@ const fetchUsersFailure=(error)=>{
 
 //reducer
 const reducer=(state=initialState,action)=>{
-   switch(Selection.type){
+   switch(action.type){
        case FETCH_USERS_REQUEST:
            return{
                ...state,
@@ -55,6 +59,21 @@ const reducer=(state=initialState,action)=>{
    }
 }
 
+//actions creators
+const fetchUsers=()=>{
+    return function(dispatch){
+        dispatch(fetchUsersRequest())
+    axios.get('https://jsonplaceholder.typicode.com/users')
+    .then(res=>{
+        const users=res.data.map(user=>user.id)
+     dispatch(fetchUsersSuccess(users))
+    }).catch(err=>{
+        dispatch(fetchUsersFailure(err.message))
+    })
+}
+}
 
 //store
-const store=createStore(reducer)
+const store=createStore(reducer,applyMiddleware(thunkMiddleware))
+store.subscribe(()=>{console.log(store.getState())})
+store.dispatch(fetchUsers())
