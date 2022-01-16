d384073e7b408a9a19f2b4c6ed59835771ac7905
Neha <nehaprajapati1150@gmail.com>
2022-01-16 21:25:47 +0530

    redux store

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+//store
+const redux=require('redux')
+const createStore=redux.createStore
+
+
 //creating action
 const BUY_CAKE='BUY_CAKE'
 
@@ -24,3 +29,23 @@ const reducer=(state=initialState,action)=>{
      default:return state
  }
 }
+
+//store holding application state
+const store=createStore(reducer)
+
+//allow access to state via getState()
+console.log('initialState',store.getState());
+
+//register listener via subscribe(listener)
+const unsubscribe=store.subscribe(()=>console.log('updated state',store.getState()))
+
+//allow state to be updated via dispatch(action)
+store.dispatch(buycake())   //buycake i.e-action
+store.dispatch(buycake())   
+store.dispatch(buycake())   
+
+//unregistering of listeners via function returned by subscribe(listener)
+//unsubscribe changes
+unsubscribe()
+
+
