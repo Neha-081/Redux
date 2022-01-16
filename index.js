236148992567470460185d30e683ca319162580a
Neha <nehaprajapati1150@gmail.com>
2022-01-16 20:37:39 +0530

    reducer and initialstate

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//creating action
 const BUY_CAKE='BUY_CAKE'
 
 function buycake(){
@@ -5,4 +6,21 @@ function buycake(){
         type:BUY_CAKE,
         info:'first redux action'
     }
+}
+
+//(previousState,action)=newState
+const initialState={
+    num_of_cakes:10
+}
+
+
+//reducer
+const reducer=(state=initialState,action)=>{
+ switch(action.type){
+     case BUY_CAKE:return{
+         ...state,
+         num_of_cakes:state.num_of_cakes-1
+     }
+     default:return state
+ }
 }
